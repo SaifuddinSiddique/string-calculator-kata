77833cd06b1aaeb3c4f0907c9973997619bd5d2f
SaifuddinSiddique <saifsiddique261@gmail.com>
2025-04-04 17:03:54 +0530

    Refactor code and improve indentation for readability

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,28 +1,50 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  it "returns 0 for an empty string" do
-    calculator = StringCalculator.new
-    expect(calculator.add("")).to eq(0)
-  end
-  it "returns the number itself for a single number" do
-    calculator = StringCalculator.new
-    expect(calculator.add("1")).to eq(1)
-  end
-  it "returns the sum of two numbers" do
-    calculator = StringCalculator.new
-    expect(calculator.add("1,2")).to eq(3)
-  end
-  it "returns sum for multiple numbers" do
-    calculator = StringCalculator.new
-    expect(calculator.add("1,2,3,4")).to eq(10)
-  end
-  it "supports custom delimiters" do
-    calculator = StringCalculator.new
-    expect(calculator.add("//;\n1;2")).to eq(3)
-  end
-  it "raises error for negative numbers" do
-    calculator = StringCalculator.new
-    expect { calculator.add("1,-2,3") }.to raise_error("Negative numbers not allowed: -2")
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    context "when input is an empty string" do
+      it "returns 0 for an empty string" do
+        expect(calculator.add("")).to eq(0)
+      end
+    end
+
+    context "when input is a single number" do
+      it "returns the number itself for a single number" do
+        expect(calculator.add("1")).to eq(1)
+      end
+    end
+
+    context "when input has two numbers" do
+      it "returns the sum of two numbers" do
+        expect(calculator.add("1,2")).to eq(3)
+      end
+    end
+
+    context "when input has multiple numbers" do
+      it "returns sum for multiple numbers" do
+        expect(calculator.add("1,2,3,4")).to eq(10)
+      end
+    end
+
+    context "when the input has a custom delimiter" do
+      it "correctly extracts the delimiter from the input string" do
+        delimiter, remaining_numbers = calculator.send(:extract_delimiter, "//;\n1;2")
+        expect(delimiter).to eq(";")
+        expect(remaining_numbers).to eq("1;2")
+      end
+
+      it "adds numbers with the custom delimiter" do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+
+    context "when negative numbers are detected" do
+      it "raises an error when multiple negative numbers are detected" do
+        expect { calculator.send(:check_for_negative_numbers, [1, -2, -3, 4]) }.to raise_error("Negative numbers not allowed: -2, -3")
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,19 +1,30 @@
 class StringCalculator
-    def add(numbers)
-      return 0 if numbers.empty?
+  def add(numbers)
+    return 0 if numbers.empty?
 
-      if numbers.start_with?("//")
-        delimiter, numbers = numbers.split("\n", 2)
-        delimiter = delimiter[2..]
-      else
-        delimiter = /,|\n/
-      end
-
-      num_list = numbers.split(delimiter).map(&:to_i)
-
-      negatives = num_list.select { |n| n < 0 }
-      raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
-
-      num_list.sum
+    if numbers.start_with?("//")
+      delimiter, numbers = extract_delimiter(numbers)
+    else
+      delimiter = /,|\n/
     end
+
+    num_list = numbers.split(delimiter).map(&:to_i)
+
+    check_for_negative_numbers(num_list)
+
+    num_list.sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    delimiter_line, numbers = numbers.split("\n", 2)
+    delimiter = delimiter_line[2..]  # Remove the "//" part
+    [delimiter, numbers]
+  end
+
+  def check_for_negative_numbers(numbers)
+    negatives = numbers.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+  end
 end
